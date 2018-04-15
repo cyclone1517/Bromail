147290b68b944612f6eb55301052d9bde0419352
cyclone <cyl1517@yeah.net>
2018-04-15 18:06:31 +0800

    创始管理员 用户名密码都是1000

--- src/ServerImpl/ServerThread.java
+++ src/ServerImpl/ServerThread.java
@@ -1,12 +1,21 @@
 package ServerImpl;
 
+import JavaBean.User;
+import JavaDao.UserDao;
+import JavaImpl.UserImpl;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ServerThread extends Thread{
 
 	private java.net.Socket client;
 	private OutputStream ops;
+	private InputStream ips;
+	private BufferedReader buffread;
 	
 	public ServerThread(java.net.Socket client) {
 		this.client = client;
@@ -14,7 +23,6 @@ public class ServerThread extends Thread{
 	
 	public void sendMsgToMe(String msg){
 		byte[] data = msg.getBytes();
-		//System.out.println("[ServerThread]sending..." + msg + "\r\n");
 		try {
 			System.out.println("[ServerThread]data is:" + data);
 			ops.write(data);
@@ -27,9 +35,14 @@ public class ServerThread extends Thread{
 	public void processChat(java.net.Socket client){
 		try {
 			ops = client.getOutputStream();
-			
-			sendMsgToMe("Welcome to pretended brothers' mailServer!\r\n");
+			ips = client.getInputStream();
+			buffread = new BufferedReader(new InputStreamReader(ips));
 
+			if (!welcomeAndLogin()){
+				client.close();
+			}
+			sendMsgToMe("\r\nYou have been logged in successfully!\r\n");
+			
 			client.close();
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -37,7 +50,29 @@ public class ServerThread extends Thread{
 		
 	}
 
-	
+	private boolean welcomeAndLogin(){
+		try {
+			sendMsgToMe("Welcome to pretended brothers' mailServer!\r\n");
+			sendMsgToMe("please enter your userId: ");
+			String userId = buffread.readLine();
+			sendMsgToMe("\r\nplease enter your password: ");
+			String password = buffread.readLine();
+			
+			UserDao userDao = new UserImpl();
+			User user = userDao.login(userId, password);
+			if(user==null){
+				sendMsgToMe("\r\nsorry no such user exists");
+				return false;
+			}
+			else{
+				return true;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
 	public void run(){
 		processChat(this.client);
 	}
