4aed36b4ed7c4cc5b5b9e4c33d2fcb8955b9b633
klqqq <645944845@qq.com>
2018-04-17 21:11:08 +0800

    写了好友的增删改查，有错的地方指出来

--- src/JavaDao/FriendDao.java
+++ src/JavaDao/FriendDao.java
@@ -1,15 +1,20 @@
 package JavaDao;
 
-public interface FriendDao {
-    //黑名单标志位默认 0不黑名 -1黑名
+import JavaBean.Friend;
 
-    public boolean addFriend(String userId, String friendId);
+public interface FriendDao {
+    static enum blackFlag {NO, YES}; //黑名单标志位默认 0不黑名 -1黑名
+
+    public boolean addFriend(String userId, String friendId, String remarkName);
 
     public boolean deleteFriend(String userId, String friendId);
 
-    public boolean updateRemarkName(String remarkName);
+    public boolean blacklistFriend(String userId, String friendId);
+    //加入权限选择，调用时参数用语句FriendDao.blackFlag.YES/NO;
+
+    public boolean updateRemarkName(String userId, String friendId, String remarkName);
     //修改备注名
 
-    public boolean searchFiend(String FriendIdOrName);
+    public Friend searchFriend(String userId, String remarkName);
     //搜索关键字可以是id可以是备注名
 }
